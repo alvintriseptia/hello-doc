d92235c1bd4805c65fdb163adbd3d58e858aeee2
alvintriseptia <alvintryseptia@gmail.com>
2022-06-09 13:20:09 +0700

    add iconTheme and remove comment

--- lib/core/preferences/colors.dart
+++ lib/core/preferences/colors.dart
@@ -7,4 +7,8 @@ class Colors {
   static const light = Color(0xFFf7fbff);
   static const primaryBlue = Color(0xFF0085ff);
   static const lightBlue = Color(0xFF85c3ff);
+  static const iconRed = Color(0xffFFB9B9);
+  static const iconYellow = Color(0xffFFCA86);
+  static const iconPurple = Color(0xffBCB3F6);
+  static const iconBlue = Color(0xff84C3FE);
 }
---
+++ lib/features/splash_screen/presentation/pages/splash_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:hello_doc/core/preferences/preferences.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('SplashScreen'),
+    );
+    ;
+  }
+}
